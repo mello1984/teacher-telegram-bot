44e96b25b5b6bd519aa176091d75d20254a067e4
Aleksandr Butakov <abutakov84@gmail.com>
2021-07-14 19:24:33 +0300

    Initial commit

---
+++ src/main/java/ru/butakov/teachertelegrambot/TeacherTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.butakov.teachertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/butakov/teachertelegrambot/TeacherTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.butakov.teachertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
